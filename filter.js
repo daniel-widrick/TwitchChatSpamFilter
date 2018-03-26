7bfa64345b3a4016c26d952036a81702601c8897
U-CAMPUS\widricd <widricd@DKW-INFTS-D105.campus.sunyit.edu>
2018-03-26 14:46:03 -0400

    New approach: just hide instead of deleting elements

--- filter.js
+++ filter.js
@@ -3,24 +3,18 @@ function widrickChatFilter() {
 	for(var x = 0; x < widrickChat.length; x++) {
 		var widrickCurMessage = widrickChat[x];
 		if(widrickCurMessage.childNodes.length > 7) {
-			widrickCurMessage.innerHTML = ""
 			widrickCurMessage.style.display = "none"
-			//widrickCurMessage.parentNode.removeChild(widrickCurMessage)
 			continue;
 		}
 		if(widrickCurMessage.innerHTML.includes("emote") ) {
-			widrickCurMessage.innerHTML = ""
 			widrickCurMessage.style.display = "none"
-			//widrickCurMessage.parentNode.removeChild(widrickCurMessage)
 			continue;
 		}
 		for(var i = 0; i < widrickCurMessage.childNodes.length; i++)
 		{
 			if(widrickCurMessage.childNodes[i].getAttribute("data-a-target") == "chat-message-text" ){
 				if(widrickCurMessage.childNodes[i].innerHTML.length < 15) {
-					widrickCurMessage.innerHTML = "";
 					widrickCurMessage.style.display = "none"
-					//widrickCurMessage.parentNode.removeChild(widrickCurMessage)
  
 				}
 			}
